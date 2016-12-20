15b63c5eb606a38e0c764cc16b39bce73731ebe0
liyin <liyin@liyindeMacBook-Pro.local>
2016-12-20 18:50:37 +0800

    change offer

--- utils/config.py
+++ utils/config.py
@@ -17,7 +17,7 @@ class Config:
 class ProductionConfig(Config):
     DEBUG = True
     MYSQL_USER = "root"
-    MYSQL_PASS = "214563"
+    MYSQL_PASS = "123456"
     MYSQL_HOST = "127.0.0.1"
     MYSQL_PORT = "3306"
     MYSQL_DB = "psms"
